44243cfeda2967520ea2bc1915926cc36edda1be
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-09-28 22:28:27 -0400

    Run every 5 mins

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import getFeeds from './feed-fetcher.js';
 
 console.log("Start App....");
 app.timer('qubyreporter', {
-    schedule: '0/1 * * * * *',
+    schedule: '0/5 * * * * *',
     runOnStartup: true,
     handler: async (myTimer, context) => {
       context.log("Running service...");
